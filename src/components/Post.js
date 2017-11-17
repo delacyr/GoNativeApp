129375b9f7bdb36ab32a467d398c520383512fbd
delacyr <delacyr@gmail.com>
2017-11-17 10:59:20 -0300

    Fixing tabspace and propTypes validation

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,53 +1,63 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
-    StyleSheet,
-    Text,
-    View,
-  } from 'react-native';
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import PropTypes from 'prop-types';
 
-const Post = ({post}) => (
-    <View style={styles.PostContainer}>
-        <Text style={styles.title}>{post.title}</Text>
-        <View style={styles.border}>
-        <Text style={styles.author}>{post.author}</Text>
-        </View>
-        <Text style={styles.description}>{post.description}</Text>
-
+const Post = ({ post }) => (
+  <View style={styles.PostContainer}>
+    <Text style={styles.title}>{post.title}</Text>
+    <View style={styles.border}>
+      <Text style={styles.author}>{post.author}</Text>
     </View>
+    <Text style={styles.description}>{post.description}</Text>
+
+  </View>
 );
 
+Post.propTypes = {
+  post: PropTypes.shape({
+    id: PropTypes.number,
+    title: PropTypes.string,
+    author: PropTypes.string,
+    description: PropTypes.string,
+  }).isRequired,
+};
+
 const styles = StyleSheet.create({
-    PostContainer: {
-        backgroundColor: '#FFFFFF',
-        marginTop: 20,
-        marginHorizontal: 20,
-        padding: 20,
-        shadowColor: '#DA6C6C',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 1.0,
-        shadowRadius: 5,
-        borderRadius: 5,
-    },
-    title: {
-        fontSize: 20,
-        textAlign: 'left',
-        color: '#333333',
-        fontWeight: 'bold'
-    },
-    author: {
-        color: '#999999',
-        textAlign: 'left',
-        paddingBottom: 10,
-    },
-    description: {
-        textAlign: 'left',
-        color: '#666666',
-        paddingTop: 20,
-    },
-    border: {
-        borderBottomWidth: 1,
-        borderColor: '#EEEEEE',
-    },
+  PostContainer: {
+    backgroundColor: '#FFFFFF',
+    marginTop: 20,
+    marginHorizontal: 20,
+    padding: 20,
+    shadowColor: '#DA6C6C',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 1.0,
+    shadowRadius: 5,
+    borderRadius: 5,
+  },
+  title: {
+    fontSize: 20,
+    textAlign: 'left',
+    color: '#333333',
+    fontWeight: 'bold'
+  },
+  author: {
+    color: '#999999',
+    textAlign: 'left',
+    paddingBottom: 10,
+  },
+  description: {
+    textAlign: 'left',
+    color: '#666666',
+    paddingTop: 20,
+  },
+  border: {
+    borderBottomWidth: 1,
+    borderColor: '#EEEEEE',
+  },
 });
 
 export default Post;
